64b508f9541bad3c0229f2f16316c9eb6ee2ebac
HadiKhai <hadikhairallah@outlook.com>
2025-01-17 15:56:37 +0200

    fix: error

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -11,7 +11,7 @@ import { SparklesIcon} from "@/components/icons";
 import Markdown from "react-markdown";
 import {Button} from "@/components/ui/button";
 import {useWriteContract} from "wagmi";
-import {Address, formatEther, parseAbi} from "viem";
+import {Address, parseAbi} from "viem";
 
 const PurePreviewMessage = ({
   message,
